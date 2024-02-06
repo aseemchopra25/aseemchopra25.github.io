4d2dc21b56714f8aa47db46ca18798ad3f645080
Aseem Chopra <aseemchopra@protonmail.com>
2024-02-06 18:51:19 +0530

    rotate yin yang

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
+import React, { useEffect, useRef } from "react";
 import { YinYang } from "./AllSvgs";
+import styled, { ThemeProvider } from "styled-components";
 
 function Header() {
     return (
       <header>
-        <YinYang width = {80} height = {80} />
+          <YinYang width = {80} height = {80} />
       </header>
     );
   }
   
   export default Header;
+
+
