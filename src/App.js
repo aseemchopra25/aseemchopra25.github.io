675b76e707c39de474c55d61d4d01f37adae52c7
Aseem Chopra <aseemchopra@protonmail.com>
2024-02-09 16:56:11 +0530

    sticky blur header

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Blog from './pages/Blog';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Layout from './components/Layout';
-
 function App() {
   return (
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import { YinYang } from "./AllSvgs";
+import React, { useEffect } from 'react';
 
-function Header() {
-    return (
-      <header>
-          {/* <YinYang width = {80} height = {80} /> */}
-      </header>
-    );
-  }
+  // This Header component is specifically for the top header with a blur effect
+  const Header = () => (
+    <div className="header top">
+      {/* Top Header Content */}
+      {/* <YinYang width = {80} height = {80} /> */}
+    </div>
+  );
   
   export default Header;
-
-
