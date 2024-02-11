161ae7d6fc6d3da7a9f4252a6cd8a986c37c979f
Aseem Chopra <aseemchopra@protonmail.com>
2024-02-11 21:43:48 +0530

    update: blog text hover effect

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import posts from './posts.json'; // Assuming you have a JSON file with your posts' metadata
 
-const Blog = () => {
+const BlogList = () => {
   return (
-    <div>
-      <h1>Index</h1>
-      <section>
-        {posts.map(post => (
-          <article key={post.slug}>
-            <Link to={`/blog/${post.slug}`}>
-              <h2>{post.title}</h2>
-              <p>{post.date}</p>
-            </Link>
-          </article>
-        ))}
-      </section>
+    <div className="blog-list">
+      <Link to="/"><span className="arrow2">↑</span><em>Index</em></Link>
+      <p><br/>Writing</p>
+      {posts.map(post => (
+        <div key={post.slug} className="post-entry">
+          <div className="post-year">{post.date.split('-')[0]}</div>
+          <Link to={`/blog/${post.slug}`} className="post-link">
+            <div className="post-title">{post.title}</div>
+            <div className="post-date">{post.date}</div>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Blog;
+export default BlogList;
